c684c5e83e9590dbd739645baabbf8d7ae870a52
Hiromi <75907477+nyan92015@users.noreply.github.com>
2024-09-20 23:42:13 +0900

    build

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
 const { Server, Origins } = require('boardgame.io/server');
-const { TicTacToe } = require('./Game');
 
 const server = Server({
+  // Provide the definitions for your game(s).
   games: [TicTacToe],
-  origins: [Origins.LOCALHOST],
+
+  // Provide the database storage class to use.
+  db: new DbConnector(),
+
+  origins: [
+    // Allow your game site to connect.
+    'https://bg-tutorial-3nouersax-nyan92015s-projects.vercel.app',
+    // Allow localhost to connect, except when NODE_ENV is 'production'.
+    Origins.LOCALHOST_IN_DEVELOPMENT
+  ],
 });
 
 server.run(8000);
