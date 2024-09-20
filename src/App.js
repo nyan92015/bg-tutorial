6895d307f7b5ed7cec9bde00cc40f1d0a5b565e7
Hiromi <75907477+nyan92015@users.noreply.github.com>
2024-09-21 00:50:29 +0900

    build

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Lobby } from 'boardgame.io/react';
-import { TicTacToeBoard } from './Board';
+import { Client } from 'boardgame.io/react';
+import { SocketIO } from 'boardgame.io/multiplayer'
 import { TicTacToe } from './Game';
+import { TicTacToeBoard } from './Board';
 
-const server = `bg-tutorial.railway.internal`;
-const importedGames = [{ game: TicTacToe, board: TicTacToeBoard }];
+const TicTacToeClient = Client({
+  game: TicTacToe,
+  board: TicTacToeBoard,
+multiplayer: SocketIO({ server: 'bg-tutorial.railway.internal' }),
+});
 
-export default () => (
+const App = () => (
   <div>
-    <h1>Lobby</h1>
-    <Lobby gameServer={server} lobbyServer={server} gameComponents={importedGames} />
+ <TicTacToeClient playerID="0" />
+ <TicTacToeClient playerID="1" />
   </div>
 );
+
+export default App;
