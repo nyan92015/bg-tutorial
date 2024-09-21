ca6424292f5993b748f3cbd40ebaf6d07487c391
Hiromi <75907477+nyan92015@users.noreply.github.com>
2024-09-21 12:26:05 +0900

    build

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import { Client } from 'boardgame.io/react';
-import { SocketIO } from 'boardgame.io/multiplayer'
 import { TicTacToe } from './Game';
 import { TicTacToeBoard } from './Board';
 
-const TicTacToeClient = Client({
-  game: TicTacToe,
-  board: TicTacToeBoard,
-multiplayer: SocketIO({ server: 'https://bg-tutorial.onrender.com' }),
-});
 
 const App = () => (
-  <div>
- <TicTacToeClient playerID="0" />
- <TicTacToeClient playerID="1" />
-  </div>
+ <Lobby
+  gameServer={`https://bg-tutorial.onrender.com`}
+  lobbyServer={`https://bg-tutorial.onrender.com`}
+  gameComponents={[
+    { game: TicTacToe, board: TicTacToeBoard }
+  ]}
+/>
 );
 
 export default App;
--- src/server.js
+++ src/server.js
@@ -1,25 +1,8 @@
 // server.js
-
 import { Server } from 'boardgame.io/server';
-import path from 'path';
-import serve from 'koa-static';
 import { TicTacToe } from './Game';
 
-const server = Server({ 
-    games: [TicTacToe],
-    origins: ['https://bg-tutorial.onrender.com'] 
-});
+const server = Server({ games: [TicTacToe]});
 const PORT = process.env.PORT || 8000;
 
-// Build path relative to the server.js file
-const frontEndAppBuildPath = path.resolve(__dirname, './build');
-server.app.use(serve(frontEndAppBuildPath))
-
-server.run(PORT, () => {
-  server.app.use(
-    async (ctx, next) => await serve(frontEndAppBuildPath)(
-      Object.assign(ctx, { path: 'index.html' }),
-      next
-    )
-  )
-});
+server.run(PORT);
