96cc508822192565692c61da53682bf2e4e947c4
Hiromi <75907477+nyan92015@users.noreply.github.com>
2024-09-21 19:07:17 +0900

    add match func

--- src/server.js
+++ src/server.js
@@ -1,7 +1,32 @@
+// server.js
 import { Server } from 'boardgame.io/server';
+import path from 'path';
+import serve from 'koa-static';
+import Koa from 'koa';
 import { TicTacToe } from './Game';
 
+const app = new Koa();
 const server = Server({ games: [TicTacToe] });
-const PORT = Number(process.env.PORT) || 8000; // ここで数値に変換
+const PORT = process.env.PORT || 8000;
 
-server.run(PORT);
+// ビルドされたフロントエンドのパスを設定
+const frontEndAppBuildPath = path.resolve(__dirname, './build');
+
+// 静的ファイルを提供
+app.use(serve(frontEndAppBuildPath));
+
+// ゲームサーバーのミドルウェアを設定
+app.use(server.app);
+
+// 最後にフロントエンドのindex.htmlを返すミドルウェア
+app.use(async (ctx, next) => {
+  await serve(frontEndAppBuildPath)(ctx, async () => {
+    ctx.path = 'index.html';
+    await next();
+  });
+});
+
+// サーバーを起動
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
